49119575dc29b1b583bff291976cc55eaa8fc0f5
sagar masale <128501915+Sagar-masale@users.noreply.github.com>
2023-06-09 20:12:24 +0530

    script

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text",{
+    strings:["Full stack Developer","Learn DSA",
+    "GuitaRist",],
+    typeSpeed:70,
+    backSpeed:50,
+    backDelay:1500,
+    loop:true
+})
